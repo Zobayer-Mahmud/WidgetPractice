639806463a2f3e9145d916d826e93ff138a4b142
Zobayer-Mahmud99 <zobayer.mahmud@parallaxlogic.net>
2022-12-26 17:46:12 +0600

    animated Position

---
+++ lib/animated_rotation.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AnimatedRotationWidget extends StatefulWidget {
+  const AnimatedRotationWidget({Key? key}) : super(key: key);
+
+  @override
+  State<AnimatedRotationWidget> createState() => _AnimatedRotationWidgetState();
+}
+
+class _AnimatedRotationWidgetState extends State<AnimatedRotationWidget> {
+  double turns = 0.0;
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:practice_project/animatedPosition.dart';
+import 'package:practice_project/animated_rotation.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.purple,
       ),
       debugShowCheckedModeBanner: false,
-      home: const AnimatedPositionWidget(),
+      home: const AnimatedRotationWidget(),
     );
   }
 }
