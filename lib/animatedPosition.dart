8e60271b9c0e213802020b5d975c688a9c97ae70
Zobayer-Mahmud99 <zobayer.mahmud@parallaxlogic.net>
2022-12-26 11:56:29 +0600

    animated positioned

--- lib/animatedPosition.dart
+++ lib/animatedPosition.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:practice_project/custom_Widget/customAppBar.dart';
 
 class AnimatedPositonWidget extends StatefulWidget {
   const AnimatedPositonWidget({Key? key}) : super(key: key);
@@ -9,9 +10,20 @@ class AnimatedPositonWidget extends StatefulWidget {
   State<AnimatedPositonWidget> createState() => _AnimatedPositonWidgetState();
 }
 
-class _AnimatedPositonWidgetState extends State<AnimatedPositonWidget> {
+class _AnimatedPositonWidgetState extends State<AnimatedPositonWidget> with SingleTickerProviderStateMixin {
+  bool isLeftCollapsed = true;
+  bool isRightCollapsed = true;
+  bool isTopCollapsed = true;
+  bool isBottomCollapsed = true;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: CustomAppBar(title: 'Animated Position'),
+      ),
+      body: Column(),
+    );
   }
 }
